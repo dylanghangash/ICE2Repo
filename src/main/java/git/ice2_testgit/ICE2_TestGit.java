f4ee953a8d25e4caae881fb04347755ddc80baf2
dylan ghangash <ghangasd@sheridancollege.ca>
2025-10-02 09:40:45 -0400

    rev 1 local

---
+++ src/main/java/git/ice2_testgit/ICE2_TestGit.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package git.ice2_testgit;
+
+/**
+ *
+ * @author dylan
+ */
+public class ICE2_TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("rev 1 local");
+    }
+}
